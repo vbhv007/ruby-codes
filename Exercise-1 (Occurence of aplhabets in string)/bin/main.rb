71c70a673ff571adbc246bfd02355457c0588271
vbhv007 <bhardwajvbhv@gmail.com>
2018-05-20 11:16:11 +0530

    duplicate files deleted

--- Exercise-1 (Occurence of aplhabets in string)/bin/main.rb
+++
@@ -1,7 +0,0 @@
-require '../lib/String.rb'
-
-if ARGV[0].nil?
-  puts 'Please provide an input'
-  exit
-end
-ARGV[0].count_occurence
--- Exercise-1 (Occurence of aplhabets in string)/lib/String.rb
+++
@@ -1,7 +0,0 @@
-class String
-  def count_occurence
-    hash = Hash.new(0)
-    scan(/[a-zA-Z]/).each { |a| hash[a] += 1 }
-    p hash
-  end
-end
