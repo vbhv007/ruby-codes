98b13503b78e1d10d8927bed909a0a8cba294cd4
vbhv007 <bhardwajvbhv@gmail.com>
2018-05-24 09:07:13 +0530

    Sales Tax

---
+++ Sales Tax/lib/Sales.rb
@@ -0,0 +1,20 @@
+class Sales
+  def input
+    arr = []
+    puts 'Name of the product'
+    arr << gets.chomp
+    puts 'Imported?:'
+    arr << gets.chomp
+    puts 'Exempted from sales tax?:'
+    arr << gets.chomp
+    puts 'Price:'
+    arr << gets.chomp
+    puts 'Do you want to add more items to your list(y/n):'
+    arr << gets.chomp
+    p arr
+  end
+
+  def tax(price, rate)
+    price * (1 + rate / 100)
+  end
+end
