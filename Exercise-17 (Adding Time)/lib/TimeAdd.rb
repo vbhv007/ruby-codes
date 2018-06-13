1cedbd02c1fc787289d4de558ca5e2250552c7ec
vbhv007 <bhardwajvbhv@gmail.com>
2018-06-13 17:55:08 +0530

    Adding Time

--- Exercise-17 (Adding Time)/lib/TimeAdd.rb
+++ Exercise-17 (Adding Time)/lib/TimeAdd.rb
@@ -1,20 +1,16 @@
 require 'time'
 # this is class String
 class Time
+  TIME = /^(2[0-3]|[01][0-9]|[01]):([0-5]?[0-9]):([0-5]?[0-9])$/
+
   def sum_time(time1, time2)
-    if format_time?(time1) && format_time?(time2)
-      t1 = Time.parse(time1)
-      t2 = Time.parse(time2)
-      total = t1 + t2.sec + t2.min * 60 + t2.hour * 3600
-      print_time(total, (total.day - day))
-    else
-      puts 'Invalid 24 hour time value'
-      exit
-    end
+    time1 =~ TIME && time2 =~ TIME ? total = Time.add(time1, time2) : puts('Invalid 24 hour time value')
+    print_time(total, (total.day - day))
   end
 
-  def format_time?(time)
-    time =~ /^(2[0-3]|[01][0-9]|[01]):([0-5]?[0-9]):([0-5]?[0-9])$/
+  def self.add(time1, time2)
+    t = parse(time2)
+    parse(time1) + t.sec + t.min * 60 + t.hour * 3600
   end
 
   def print_time(total, day)
