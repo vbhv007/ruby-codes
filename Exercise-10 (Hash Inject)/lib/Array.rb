1a4c617eda066cdff74bf4fec6cfeff30036ce04
vbhv007 <bhardwajvbhv@gmail.com>
2018-06-17 19:25:17 +0530

    Hash Inject

--- Exercise-10 (Hash Inject)/lib/Array.rb
+++ Exercise-10 (Hash Inject)/lib/Array.rb
@@ -1,21 +1,6 @@
 # this is class Array
 class Array
   def make_hash
-    sort_hash(sort_by(&:length).group_by(&:length))
-  end
-
-  def sort_hash(hash)
-    even, odd = [], []
-    hash.keys.each do |key|
-      key.even? ? even << hash[key] : odd << hash[key]
-    end
-    display_hash(even, odd)
-  end
-
-  def display_hash(even, odd)
-    hash = Hash.new([])
-    hash['odd'] = odd if !odd.length.zero?
-    hash['even'] = even if !even.length.zero?
-    hash
+    group_by { |z| z.to_s.length }.map{ |_, value| value }.group_by { |t| t[0].to_s.length.even? ? 'even' : 'odd' }
   end
 end
