91c7fab4cff2df92bddb84f5afcedcaa64f469b1
vbhv007 <bhardwajvbhv@gmail.com>
2018-06-17 19:26:39 +0530

    Adding Time

--- Exercise-17 (Adding Time)/bin/main.rb
+++ Exercise-17 (Adding Time)/bin/main.rb
@@ -4,4 +4,4 @@ if ARGV[0].nil?
   puts 'Please provide an input'
   exit
 end
-p Time.new.sum_time(ARGV[0], ARGV[1])
+p Time.sum_time(ARGV[0], ARGV[1])
--- Exercise-17 (Adding Time)/lib/TimeAdd.rb
+++ Exercise-17 (Adding Time)/lib/TimeAdd.rb
@@ -3,9 +3,9 @@ require 'time'
 class Time
   TIME = /^(2[0-3]|[01][0-9]|[01]):([0-5]?[0-9]):([0-5]?[0-9])$/
 
-  def sum_time(time1, time2)
-    time1 =~ TIME && time2 =~ TIME ? total = Time.add(time1, time2) : puts('Invalid 24 hour time value')
-    print_time(total, (total.day - day))
+  def self.sum_time(time1, time2)
+    time1 =~ TIME && time2 =~ TIME ? total = add(time1, time2) : puts('Invalid 24 hour time value')
+    print_time(total, (total.day - now.day))
   end
 
   def self.add(time1, time2)
@@ -13,7 +13,7 @@ class Time
     parse(time1) + t.sec + t.min * 60 + t.hour * 3600
   end
 
-  def print_time(total, day)
+  def self.print_time(total, day)
     day.zero? ? total.strftime('%T') : "#{day} day & #{total.strftime('%T')}"
   end
 end
