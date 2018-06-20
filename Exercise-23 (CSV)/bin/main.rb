03bc3a3f77add9bd48666a90689c632114626157
vbhv007 <bhardwajvbhv@gmail.com>
2018-06-20 16:00:28 +0530

    CSV

--- Exercise-23 (CSV)/bin/main.rb
+++ Exercise-23 (CSV)/bin/main.rb
@@ -1,6 +1,6 @@
 require '../lib/File.rb'
 
 ob = CsvData.new
-file =  CSV.read('../../exercise23CSV.csv', headers: false)
+file = CSV.read('../../exercise23CSV.csv')
 hash = ob.read_file(file)
 ob.save_into_file(hash.sort.to_h)
--- Exercise-23 (CSV)/lib/File.rb
+++ Exercise-23 (CSV)/lib/File.rb
@@ -2,19 +2,14 @@ require 'csv'
 # this is class CsvData
 class CsvData
   def read_file(file)
-    hash = Hash.new([])
-    file.each do |l|
-       hash[l[2]] += Array("#{l[0]} (EmpId:#{l[1]})")
-    end
-    hash
+    file.group_by { |l| l[2] }
   end
 
   def save_into_file(hash)
     File.open('../../exercise23_Output.txt', 'w') do |line|
       hash.each do |a, e|
         e.length > 1 ? line.puts("#{a}s") : line.puts(a)
-        e.each { |d| line.puts d }
-        line.puts
+        e.each { |d| line.puts "#{d[0]} (EmpId:#{d[1]})" }
       end
     end
   end
